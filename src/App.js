ff6abdb5db832cd624a2b73daafad43db8678c6f
E-Liang Tan <eliang@eliangtan.com>
2020-06-10 20:55:23 +0800

    Add useTransition demo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
-import React from "react";
+import React, {
+  Suspense,
+  useCallback,
+  useState,
+  unstable_useTransition,
+} from "react";
+
+function CounterButton() {
+  const [count, setCount] = useState(0);
+  const [startTransition, isPending] = unstable_useTransition({
+    timeoutMs: 1000,
+  });
+
+  // TODO: Figure out why this doesn't suspend :(
+
+  const handleClick = useCallback(() => {
+    startTransition(() => {
+      setTimeout(() => {
+        setCount(count + 1);
+      }, 2000);
+    });
+  }, [count, setCount, startTransition]);
+
+  return (
+    <button style={{ display: "block" }} onClick={handleClick}>
+      Button {count} {isPending}
+    </button>
+  );
+}
+
+function SuspenseDemo() {
+  return (
+    <>
+      <Suspense fallback="SUSPENDED">
+        {[...Array(5).keys()].map((idx) => (
+          <CounterButton key={idx} />
+        ))}
+      </Suspense>
+    </>
+  );
+}
 
 function App() {
-  return (
-    <div>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <SuspenseDemo />;
 }
 
 export default App;
