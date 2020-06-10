87fe9cc47fccbec75e86890c94bfc939d94783a7
E-Liang Tan <eliang@eliangtan.com>
2020-06-10 16:01:55 +0800

    Nuke assets and wrap everything in debug tracing mode

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { unstable_createRoot } from "react-dom";
-import "./index.css";
 import App from "./App";
 
 unstable_createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <React.unstable_DebugTracingMode>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </React.unstable_DebugTracingMode>
 );
